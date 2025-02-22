3c391a3310200ad1d202729b1fa780ba9a8b7ad1
pranam-s <pranamsrivastava28@gmail.com>
2025-02-22 22:17:28 +0530

    cors setup

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine
 from models import Base
 from routes import router
@@ -10,6 +11,13 @@ import logging
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(router)
 
 @app.on_event("startup")
@@ -22,3 +30,4 @@ def startup_event():
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+```
